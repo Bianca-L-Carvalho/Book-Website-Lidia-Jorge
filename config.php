9216efa60012740c2682cac58514dd3fbbae3e10
Bianca Carvalho <carvalho.bianca@hotmail.com>
2023-01-31 19:49:40 +0000

    git init

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+define('PAGE_TITLE', 'Lídia Jorge');
+
+
+define('PAGE_URL', 'http://localhost/site_lidiaJorge/'); 
+
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'lidia.jorge');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_PORT', '3306');
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+date_default_timezone_set('Europe/London');
